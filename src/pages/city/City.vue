a1af1f0449f90cb79693646920570a55c64c1872
jangzeen <1363646276@qq.com>
2019-08-26 10:53:22 +0800

    complete city header

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <city-header></city-header>
+    </div>
+</template>
+
+<script>
+import CityHeader from './component/CityHeader'
+export default {
+  name: 'City',
+  components: {CityHeader},
+  comments: {CityHeader}
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/city/component/CityHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header">
+      城市选择
+        <router-link to="/">
+          <div class="iconfont city-back">&#xe624;</div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    background:  #00bcd4;
+    position relative
+    height .86rem
+    line-height .86rem
+    color #fff
+    text-align center
+    font-size .36rem
+    .city-back
+      width .7rem
+      text-align center
+      position absolute
+      top 0
+      left 0
+      color #fff
+      line-height .86rem
+      font-size .4rem
+</style>
